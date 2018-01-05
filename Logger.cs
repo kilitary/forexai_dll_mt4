74bbb6be5afedfe2464942350fc68ecc5a242aad
deconf <deconf@ya.ru>
2018-01-06 00:24:38 +0300

    changes

--- Logger.cs
+++ Logger.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,6 +37,106 @@ namespace forexAI
             System.IO.StreamWriter file = new System.IO.StreamWriter(@"d:\temp\forexAI\mt4.log", true);
             file.WriteLine("error: " + lines);
             file.Close();
+        }
+
+        public static string dump(object value, string name = "unknown", int depth = 5, bool show = false)
+        {
+            StringBuilder result = new StringBuilder();
+
+            string spaces = "|   ";
+            string indent = new StringBuilder().Insert(0, spaces, depth).ToString();
+            string displayValue = String.Empty;
+
+            try
+            {
+                if (value != null)
+                {
+                    Type type = value.GetType();
+
+                    displayValue = value.ToString();
+
+                    if (value is Boolean)
+                    {
+                        displayValue = String.Format("{0}({1})", type.FullName, value.Equals(true) ? "True" : "False");
+                    }
+                    else if (value is Int16 || value is UInt16 ||
+                             value is Int32 || value is UInt32 ||
+                             value is Int64 || value is UInt64 ||
+                             value is Single || value is Double ||
+                             value is Decimal)
+                    {
+                        displayValue = String.Format("{0}({1})", type.FullName, displayValue);
+                    }
+                    else if (value is Char)
+                    {
+                        displayValue = String.Format("{0} \"{1}\"", type.FullName, displayValue);
+                    }
+                    else if (value is Char[])
+                    {
+                        displayValue = String.Format("{0}(#{1}) \"{2}\"", type.FullName, (value as Char[]).Length,
+                            displayValue);
+                    }
+                    else if (value is String)
+                    {
+                        displayValue = String.Format("{0}(#{1}) \"{2}\"", type.FullName, (value as String).Length,
+                            displayValue);
+                    }
+                    else if (value is Byte || value is SByte)
+                    {
+                        displayValue = String.Format("{0} 0x{1:X}", type.FullName, value);
+                    }
+                    else if (value is Byte[] || value is SByte[])
+                    {
+                        displayValue = String.Format("{0}(#{1}) 0x{2}", type.FullName, (value as Byte[]).Length, BitConverter.ToString(value as Byte[]));
+                    }
+                    else if (value is ICollection)
+                    {
+                        var i = 0;
+                        var displayValues = String.Empty;
+
+                        var collection = value as ICollection;
+                        foreach (object element in collection)
+                        {
+                            displayValues = String.Concat(displayValues, dump(element, i.ToString(), depth + 1, false));
+                            i++;
+                        }
+
+                        displayValue = String.Format("{0}(#{1}) {{\n{2}{3}}}", type.FullName, collection.Count, displayValues, indent);
+                    }
+                    else
+                    {
+                        var displayValues = String.Empty;
+
+                        PropertyInfo[] properties = type.GetProperties();
+                        foreach (PropertyInfo property in properties)
+                        {
+                            displayValues = String.Concat(displayValues,
+                                dump(property.GetValue(value, null), property.Name, depth + 1, false));
+                        }
+
+                        displayValue = String.Format("{0}(#{1}) {{\n{2}{3}}}\n", type.Name, properties.Length, displayValues, indent);
+                    }
+                }
+                else
+                {
+                    displayValue = "null";
+                }
+
+                if (name != String.Empty)
+                {
+                    result.Append(indent + ' ' + name + " => " + displayValue + "\n");
+                }
+                else
+                {
+                    result.Append(indent + displayValue + "\n");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return result.ToString();
         }
     }
 }
--- forexAI.cs
+++ forexAI.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static forexAI.Logger;
+using FANNCSharp.Double;
 
 namespace forexAI
 {
@@ -18,10 +19,24 @@ namespace forexAI
     public class ForexAI : MqlApi
     {
         private const int MAGICMA = 20050610;
-        private double DecreaseFactor = 3;
-        private double MaximumRisk = 0.02;
-        private int MovingPeriod = 12;
-        private int MovingShift = 6;
+
+        //private double DecreaseFactor = 3;
+        //private double MaximumRisk = 0.02;
+        //private int MovingPeriod = 12;
+        //private int MovingShift = 6;
+        private int previousBars = 0;
+
+        public Random random = new Random();
+
+        public void LoadNetwork(string dirName)
+        {
+            log($"Loading fann network [{dirName}]");
+
+            NeuralNet ai = new NeuralNet($"d:\\temp\\forexAI\\{dirName}\\FANN.net");
+
+            log($"network: hash={ai.GetHashCode()} inputs={ai.InputCount} outputs={ai.OutputCount} neurons={ai.TotalNeurons}");
+            log($"net={dump(ai)}");
+        }
 
         public override int deinit()
         {
@@ -40,8 +55,12 @@ namespace forexAI
             int num = 0;
             foreach (DirectoryInfo file in Files)
             {
-                info($"network #{num}  {file.Name}");
-                num++;
+                info($"network #{num++} {file.Name}");
+                if (random.Next(2) == 1)
+                {
+                    LoadNetwork(file.Name);
+                    break;
+                }
             }
 
             info("done");
@@ -54,16 +73,16 @@ namespace forexAI
         public override int start()
         {
             //---- check for history and trading
-            if (Bars < 100 || !IsTradeAllowed())
+            if (Bars == previousBars)
                 return 0;
+            previousBars = Bars;
+            ////---- calculate open orders by current symbol
+            //string symbol = Symbol();
 
-            //---- calculate open orders by current symbol
-            string symbol = Symbol();
-
-            if (CalculateCurrentOrders() == 0)
-                CheckForOpen(symbol);
-            else
-                CheckForClose(symbol);
+            //if (CalculateCurrentOrders() == 0)
+            //    CheckForOpen(symbol);
+            //else
+            //    CheckForClose(symbol);
 
             return 0;
         }
@@ -71,129 +90,129 @@ namespace forexAI
         //+------------------------------------------------------------------+
         //| Calculate open positions                                         |
         //+------------------------------------------------------------------+
-        private int CalculateCurrentOrders()
-        {
-            int buys = 0, sells = 0;
+        //private int CalculateCurrentOrders()
+        //{
+        //    int buys = 0, sells = 0;
 
-            for (int i = 0; i < OrdersTotal(); i++)
-            {
-                if (!OrderSelect(i, SELECT_BY_POS, MODE_TRADES))
-                    break;
-                if (OrderSymbol() == Symbol() && OrderMagicNumber() == MAGICMA)
-                {
-                    if (OrderType() == OP_BUY)
-                        buys++;
-                    if (OrderType() == OP_SELL)
-                        sells++;
-                }
-            }
+        //    for (int i = 0; i < OrdersTotal(); i++)
+        //    {
+        //        if (!OrderSelect(i, SELECT_BY_POS, MODE_TRADES))
+        //            break;
+        //        if (OrderSymbol() == Symbol() && OrderMagicNumber() == MAGICMA)
+        //        {
+        //            if (OrderType() == OP_BUY)
+        //                buys++;
+        //            if (OrderType() == OP_SELL)
+        //                sells++;
+        //        }
+        //    }
 
-            //---- return orders volume
-            if (buys > 0)
-                return (buys);
-            return (-sells);
-        }
+        //    //---- return orders volume
+        //    if (buys > 0)
+        //        return (buys);
+        //    return (-sells);
+        //}
 
-        //+------------------------------------------------------------------+
-        //| Check for close order conditions                                 |
-        //+------------------------------------------------------------------+
-        private void CheckForClose(string symbol)
-        {
-            //---- go trading only for first tiks of new bar
-            if (Volume[0] > 1)
-                return;
-            //---- get Moving Average
-            double ma = iMA(symbol, 0, MovingPeriod, MovingShift, MODE_SMA, PRICE_CLOSE, 0);
+        ////+------------------------------------------------------------------+
+        ////| Check for close order conditions                                 |
+        ////+------------------------------------------------------------------+
+        //private void CheckForClose(string symbol)
+        //{
+        //    //---- go trading only for first tiks of new bar
+        //    if (Volume[0] > 1)
+        //        return;
+        //    //---- get Moving Average
+        //    double ma = iMA(symbol, 0, MovingPeriod, MovingShift, MODE_SMA, PRICE_CLOSE, 0);
 
-            Print("orders " + OrdersTotal());
+        //    Print("orders " + OrdersTotal());
 
-            for (int i = 0; i < OrdersTotal(); i++)
-            {
-                if (!OrderSelect(i, SELECT_BY_POS, MODE_TRADES))
-                    break;
-                if (OrderMagicNumber() != MAGICMA || OrderSymbol() != Symbol())
-                    continue;
-                //---- check order type
-                if (OrderType() == OP_BUY)
-                {
-                    log("test  bar " + Bars + " on " + symbol + " balance:" + AccountBalance() + " profit=" + OrderProfit());
-                    if (Open[1] > ma && Close[1] < ma)
-                        OrderClose(OrderTicket(), OrderLots(), Bid, 3, Color.White);
+        //    for (int i = 0; i < OrdersTotal(); i++)
+        //    {
+        //        if (!OrderSelect(i, SELECT_BY_POS, MODE_TRADES))
+        //            break;
+        //        if (OrderMagicNumber() != MAGICMA || OrderSymbol() != Symbol())
+        //            continue;
+        //        //---- check order type
+        //        if (OrderType() == OP_BUY)
+        //        {
+        //            log("test  bar " + Bars + " on " + symbol + " balance:" + AccountBalance() + " profit=" + OrderProfit());
+        //            if (Open[1] > ma && Close[1] < ma)
+        //                OrderClose(OrderTicket(), OrderLots(), Bid, 3, Color.White);
 
-                    break;
-                }
-                if (OrderType() == OP_SELL)
-                {
-                    log("test  bar " + Bars + " on " + symbol + " balance:" + AccountBalance() + " profit=" + OrderProfit());
-                    if (Open[1] < ma && Close[1] > ma)
-                        OrderClose(OrderTicket(), OrderLots(), Ask, 3, Color.White);
+        //            break;
+        //        }
+        //        if (OrderType() == OP_SELL)
+        //        {
+        //            log("test  bar " + Bars + " on " + symbol + " balance:" + AccountBalance() + " profit=" + OrderProfit());
+        //            if (Open[1] < ma && Close[1] > ma)
+        //                OrderClose(OrderTicket(), OrderLots(), Ask, 3, Color.White);
 
-                    break;
-                }
-            }
-        }
+        //            break;
+        //        }
+        //    }
+        //}
 
-        //+------------------------------------------------------------------+
-        //| Check for open order conditions                                  |
-        //+------------------------------------------------------------------+
-        private void CheckForOpen(string symbol)
-        {
-            //---- go trading only for first tiks of new bar
-            if (Volume[0] > 1)
-                return;
+        ////+------------------------------------------------------------------+
+        ////| Check for open order conditions                                  |
+        ////+------------------------------------------------------------------+
+        //private void CheckForOpen(string symbol)
+        //{
+        //    //---- go trading only for first tiks of new bar
+        //    if (Volume[0] > 1)
+        //        return;
 
-            //---- get Moving Average
-            double ma = iMA(symbol, 0, MovingPeriod, MovingShift, MODE_SMA, PRICE_CLOSE, 0);
+        //    //---- get Moving Average
+        //    double ma = iMA(symbol, 0, MovingPeriod, MovingShift, MODE_SMA, PRICE_CLOSE, 0);
 
-            //---- sell conditions
-            if (Open[1] > ma && Close[1] < ma)
-            {
-                OrderSend(Symbol(), OP_SELL, LotsOptimized(), Bid, 3, 0, 0, "", MAGICMA, DateTime.MinValue, Color.Red);
-                return;
-            }
-            //---- buy conditions
-            if (Open[1] < ma && Close[1] > ma)
-            {
-                OrderSend(Symbol(), OP_BUY, LotsOptimized(), Ask, 3, 0, 0, "", MAGICMA, DateTime.MinValue, Color.Blue);
-            }
-        }
+        //    //---- sell conditions
+        //    if (Open[1] > ma && Close[1] < ma)
+        //    {
+        //        OrderSend(Symbol(), OP_SELL, LotsOptimized(), Bid, 3, 0, 0, "", MAGICMA, DateTime.MinValue, Color.Red);
+        //        return;
+        //    }
+        //    //---- buy conditions
+        //    if (Open[1] < ma && Close[1] > ma)
+        //    {
+        //        OrderSend(Symbol(), OP_BUY, LotsOptimized(), Ask, 3, 0, 0, "", MAGICMA, DateTime.MinValue, Color.Blue);
+        //    }
+        //}
 
-        //+------------------------------------------------------------------+
-        //| Calculate optimal lot size                                       |
-        //+------------------------------------------------------------------+
-        private double LotsOptimized()
-        {
-            // history orders total
-            int orders = OrdersHistoryTotal();
-            // number of losses orders without a break
-            int losses = 0;
-            //---- select lot size
-            double lot = NormalizeDouble(AccountFreeMargin() * MaximumRisk / 1000.0, 1);
-            //---- calcuulate number of losses orders without a break
-            if (DecreaseFactor > 0)
-            {
-                for (int i = orders - 1; i >= 0; i--)
-                {
-                    if (!OrderSelect(i, SELECT_BY_POS, MODE_HISTORY))
-                    {
-                        Print("Error in history!");
-                        break;
-                    }
-                    if (OrderSymbol() != Symbol() || OrderType() > OP_SELL)
-                        continue;
+        ////+------------------------------------------------------------------+
+        ////| Calculate optimal lot size                                       |
+        ////+------------------------------------------------------------------+
+        //private double LotsOptimized()
+        //{
+        //    // history orders total
+        //    int orders = OrdersHistoryTotal();
+        //    // number of losses orders without a break
+        //    int losses = 0;
+        //    //---- select lot size
+        //    double lot = NormalizeDouble(AccountFreeMargin() * MaximumRisk / 1000.0, 1);
+        //    //---- calcuulate number of losses orders without a break
+        //    if (DecreaseFactor > 0)
+        //    {
+        //        for (int i = orders - 1; i >= 0; i--)
+        //        {
+        //            if (!OrderSelect(i, SELECT_BY_POS, MODE_HISTORY))
+        //            {
+        //                Print("Error in history!");
+        //                break;
+        //            }
+        //            if (OrderSymbol() != Symbol() || OrderType() > OP_SELL)
+        //                continue;
 
-                    if (OrderProfit() > 0)
-                        break;
-                    if (OrderProfit() < 0)
-                        losses++;
-                }
-                if (losses > 1)
-                    lot = NormalizeDouble(lot - lot * losses / DecreaseFactor, 1);
-            }
-            //---- return lot size
-            if (lot < 0.1)
-                lot = 0.1;
-            return (lot);
-        }
+        //            if (OrderProfit() > 0)
+        //                break;
+        //            if (OrderProfit() < 0)
+        //                losses++;
+        //        }
+        //        if (losses > 1)
+        //            lot = NormalizeDouble(lot - lot * losses / DecreaseFactor, 1);
+        //    }
+        //    //---- return lot size
+        //    if (lot < 0.1)
+        //        lot = 0.1;
+        //    return (lot);
+        //}
     }
 }
