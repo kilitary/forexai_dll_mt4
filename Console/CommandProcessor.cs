b7d2db1a8d8f54a5df8c39190b7e1a6688794f5f
deconf <kilitary@gmail.com>
2018-10-01 02:25:05 +0300

    dev

--- Console/CommandProcessor.cs
+++ Console/CommandProcessor.cs
@@ -10,188 +10,200 @@ using System.IO;
 
 namespace forexAI
 {
-	public static class ConsoleCommandReceiver
-	{
-		[DllImport("kernel32.dll")]
-		private static extern bool FreeConsole();
+   public static class ConsoleCommandReceiver
+   {
+      [DllImport("kernel32.dll")]
+      private static extern bool FreeConsole();
 
-		static string[] commandLineParts = null;
-		static string typing = String.Empty;
-		static string resultString = string.Empty;
-		static string command = string.Empty;
-		static bool runCommandLineParser = true;
+      static string[] commandLineParts = null;
+      static string typing = String.Empty;
+      static string resultString = string.Empty;
+      static string command = string.Empty;
+      static bool bRunCommandLineParser = true;
 
-		public static void CommandsReadingLoop()
-		{
-			Console.CursorLeft = 0;
-			Console.CursorTop = 0;
-			Console.Beep(1650, 33);
+      public static void RotateCommand(bool bSave = false)
+      {
+         if(App.currentNetworkId.Length <= 0)
+            resultString = $"empty current network dir";
+         else if(bSave && Directory.Exists(Configuration.rootDirectory + $"\\{App.currentNetworkId}"))
+            Directory.Move(Configuration.rootDirectory + "\\" + App.currentNetworkId,
+               Configuration.rootDirectory + "\\SELECTED\\" + App.currentNetworkId);
+         else if(bSave == false && Directory.Exists(Configuration.rootDirectory + $"\\{App.currentNetworkId}"))
+            Directory.Delete(Configuration.rootDirectory + "\\" + App.currentNetworkId, true);
 
-			while(runCommandLineParser)
-			{
-				try
-				{
-					typing = ReadLine().Trim();
-					commandLineParts = typing.Split(' ');
-					resultString = string.Empty;
+         var dirs = new DirectoryInfo(Configuration.rootDirectory + "\\NEW").GetDirectories("NET_*");
+         var rnd = YRandom.between(0, dirs.Length - 1);
 
-					if(commandLineParts.Count() <= 0 || commandLineParts[0].Length == 0)
-						continue;
+         if(dirs.Length > 0)
+         {
+            resultString = $"next dir chosen (from {dirs.Length} dirs): {dirs[rnd]}";
 
-					log($"command: {typing}", "dev");
-					consolelog($"=> {typing}", "dev", ConsoleColor.Gray);
+            try
+            {
+               Directory.Move(Configuration.rootDirectory + $"\\NEW\\{dirs[rnd]}",
+                  Configuration.rootDirectory + $"\\{dirs[rnd]}");
+            }
+            catch(Exception e)
+            {
+               consolelog($"exception: {e.Message}");
+            }
+         }
+         else
+         {
+            resultString = "no new networks";
+         }
 
-					command = commandLineParts[0].ToLower();
+         Beep(1000, 20);
+      }
 
-					switch(command)
-					{
-						case "exit":
-							consolelog($"exit called");
-							Environment.Exit(0);
-							break;
+      public static void CommandsReadingLoop()
+      {
+         Console.CursorLeft = 0;
+         Console.CursorTop = 0;
+         Console.Beep(1650, 33);
 
-						case "break":
-							consolelog($"next optimization step called");
-							App.mqlApi.ExpertRemove();
-							break;
+         while(bRunCommandLineParser)
+         {
+            try
+            {
+               typing = ReadLine().Trim();
+               commandLineParts = typing.Split(' ');
+               resultString = string.Empty;
 
-						case "rotate":
-							if(App.currentNetworkId.Length <= 0)
-								resultString = $"empty current network dir";
-							else if(Directory.Exists(Configuration.rootDirectory + $"\\{App.currentNetworkId}"))
-								Directory.Delete(Configuration.rootDirectory + "\\" + App.currentNetworkId, true);
+               if(commandLineParts.Count() <= 0 || commandLineParts[0].Length == 0)
+                  continue;
 
-							var dirs = new DirectoryInfo(Configuration.rootDirectory + "\\NEW").GetDirectories("NET_*");
-							var rnd = YRandom.between(0, dirs.Length - 1);
+               log($"command: {typing}", "dev");
+               consolelog($"=> {typing}", "dev", ConsoleColor.Gray);
 
-                     if(dirs.Length > 0)
+               command = commandLineParts[0].ToLower();
+
+               switch(command)
+               {
+                  case "exit":
+                     consolelog($"exit called");
+                     Environment.Exit(0);
+                     break;
+
+                  case "break":
+                     consolelog($"next optimization step called");
+                     App.mqlApi.ExpertRemove();
+                     break;
+
+                  case "next":
+                     RotateCommand(false);
+                     break;
+
+                  case "rotate":
+                     RotateCommand(true);
+                     break;
+
+                  case "memstats":
+                     Helpers.ShowMemoryUsage();
+                     break;
+
+                  case "testerstats":
+                     for(var i = 0; i < 100; i++)
+                        resultString += $"{i,-4}: {App.mqlApi.TesterStatistics(i)}\r\n";
+                     break;
+
+                  case "clear":
+                     Clear();
+                     break;
+
+                  case "enable":
+                     App.config.Enable(commandLineParts[1]);
+                     resultString = $"enabled {commandLineParts[1]}";
+                     break;
+
+                  case "disable":
+                     App.config.Disable(commandLineParts[1]);
+                     resultString = $"disabled {commandLineParts[1]}";
+                     break;
+
+                  case "toggle":
+                     bool enabled = App.config.Toggle(commandLineParts[1]);
+                     resultString = $"toggle {commandLineParts[1]}: {enabled}";
+                     break;
+
+                  case "version":
+                     resultString = $"version: {App.version.ToString()}";
+                     break;
+
+                  case "set":
+                  case "config":
+                     if(commandLineParts.Count() >= 1)
                      {
-                        resultString = $"next dir chosen (from {dirs.Length} dirs): {dirs[rnd]}";
+                        if(commandLineParts.Count() == 1)
+                           resultString = $"config: {App.config.DumpString()}";
+                        else
+                        {
+                           switch(commandLineParts[1].ToLower())
+                           {
+                              case "del":
+                              case "remove":
+                              case "unset":
+                              case "-":
+                                 App.config.Remove(commandLineParts[2].Trim());
+                                 resultString = $"removed {commandLineParts[2].Trim()}";
+                                 break;
 
-                        try
-                        {
-                           Directory.Move(Configuration.rootDirectory + $"\\NEW\\{dirs[rnd]}",
-                              Configuration.rootDirectory + $"\\{dirs[rnd]}");
-                        }
-                        catch(Exception e)
-                        {
-                           consolelog($"exception: {e.Message}");
+                              case "save":
+                                 var nBytes = App.config.Save();
+                                 resultString = $"config saved ({nBytes} bytes)";
+                                 break;
+
+                              case "reset":
+                              case "clear":
+                                 App.config.Clear();
+                                 resultString = $"config cleared";
+                                 break;
+
+                              default:
+                                 if(commandLineParts.Count() > 2)
+                                 {
+                                    log($"setting {commandLineParts[1].Trim()} to {commandLineParts[2].Trim()}", "dev");
+                                    App.config[commandLineParts[1].Trim()] = commandLineParts[2].Trim();
+                                    resultString = $"set {commandLineParts[1].Trim()} = {commandLineParts[2].Trim()}";
+                                    App.config.Save();
+                                 }
+                                 else
+                                 {
+                                    log($"app.config[{commandLineParts[1].Trim()}", "dev");
+                                    resultString = $"{commandLineParts[1].Trim()} = {App.config[commandLineParts[1].Trim()]}";
+                                 }
+                                 break;
+                           }
                         }
                      }
                      else
-                     {
-                        resultString = "no new networks";
-                     }
+                        resultString = $"no config cmd";
+                     break;
 
-							Beep(1000, 20);
-							break;
+                  default:
+                     resultString = $"unknown command '{command}'";
+                     break;
 
-						case "memstats":
-							Helpers.ShowMemoryUsage();
-							break;
+               }
 
-						case "testerstats":
-							for(var i = 0; i < 100; i++)
-								resultString += $"{i,-4}: {App.mqlApi.TesterStatistics(i)}\r\n";
-							break;
+               if(resultString.Trim().Contains("unknown command"))
+                  Beep(190, 15);
+               else
+                  Beep(1850, 55);
 
-						case "clear":
-							Clear();
-							break;
-
-						case "enable":
-							App.config.Enable(commandLineParts[1]);
-							resultString = $"enabled {commandLineParts[1]}";
-							break;
-
-						case "disable":
-							App.config.Disable(commandLineParts[1]);
-							resultString = $"disabled {commandLineParts[1]}";
-							break;
-
-						case "toggle":
-							bool enabled = App.config.Toggle(commandLineParts[1]);
-							resultString = $"toggle {commandLineParts[1]}: {enabled}";
-							break;
-
-						case "version":
-							resultString = $"version: {App.version.ToString()}";
-							break;
-
-						case "set":
-						case "config":
-							if(commandLineParts.Count() >= 1)
-							{
-								if(commandLineParts.Count() == 1)
-									resultString = $"config: {App.config.DumpString()}";
-								else
-								{
-									switch(commandLineParts[1].ToLower())
-									{
-										case "del":
-										case "remove":
-										case "unset":
-										case "-":
-											App.config.Remove(commandLineParts[2].Trim());
-											resultString = $"removed {commandLineParts[2].Trim()}";
-											break;
-
-										case "save":
-											var nBytes = App.config.Save();
-											resultString = $"config saved ({nBytes} bytes)";
-											break;
-
-										case "reset":
-										case "clear":
-											App.config.Clear();
-											resultString = $"config cleared";
-											break;
-
-										default:
-											if(commandLineParts.Count() > 2)
-											{
-												log($"setting {commandLineParts[1].Trim()} to {commandLineParts[2].Trim()}", "dev");
-												App.config[commandLineParts[1].Trim()] = commandLineParts[2].Trim();
-												resultString = $"set {commandLineParts[1].Trim()} = {commandLineParts[2].Trim()}";
-												App.config.Save();
-											}
-											else
-											{
-												log($"app.config[{commandLineParts[1].Trim()}", "dev");
-												resultString = $"{commandLineParts[1].Trim()} = {App.config[commandLineParts[1].Trim()]}";
-											}
-											break;
-									}
-								}
-							}
-							else
-								resultString = $"no config cmd";
-							break;
-
-						default:
-							resultString = $"unknown command '{command}'";
-							break;
-
-					}
-
-					if(resultString.Trim().Contains("unknown command"))
-						Beep(190, 15);
-					else
-						Beep(1850, 55);
-
-					if(resultString.Trim().Length > 0)
-						consolelog($"<= {resultString.Trim()}", "dev", ConsoleColor.White);
-				}
-				catch(Exception e)
-				{
-					log($"EXCEPTION COMMAND '{Newtonsoft.Json.JsonConvert.SerializeObject(commandLineParts)}: '{e.Message}: {e.StackTrace}", "error");
-				}
-				finally
-				{
-					//FreeConsole();
-					//log($"console freed", "dev");
-				}
-			}
-		}
-	}
+               if(resultString.Trim().Length > 0)
+                  consolelog($"<= {resultString.Trim()}", "dev", ConsoleColor.White);
+            }
+            catch(Exception e)
+            {
+               log($"EXCEPTION COMMAND '{Newtonsoft.Json.JsonConvert.SerializeObject(commandLineParts)}: '{e.Message}: {e.StackTrace}", "error");
+            }
+            finally
+            {
+               //FreeConsole();
+               //log($"console freed", "dev");
+            }
+         }
+      }
+   }
 }
--- Storages/Config.cs
+++ Storages/Config.cs
@@ -18,158 +18,158 @@ using static forexAI.Logger;
 
 namespace forexAI
 {
-	public class Config
-	{
-		private Dictionary<string, string> _config = new Dictionary<string, string>();
+   public class Config
+   {
+      private Dictionary<string, string> _config = new Dictionary<string, string>();
 
-		public void Inc(string logicName)
-		{
-			int current = 0;
+      public void Inc(string logicName)
+      {
+         int current = 0;
 
-			int.TryParse(this[logicName], out current);
-			current++;
-			this[logicName] = current.ToString();
-		}
+         int.TryParse(this[logicName], out current);
+         current++;
+         this[logicName] = current.ToString();
+      }
 
-		public void Dec(string logicName)
-		{
-			int current = 0;
+      public void Dec(string logicName)
+      {
+         int current = 0;
 
-			int.TryParse(this[logicName], out current);
-			current--;
-			this[logicName] = current.ToString();
-		}
+         int.TryParse(this[logicName], out current);
+         current--;
+         this[logicName] = current.ToString();
+      }
 
-		public bool IsEnabled(string logicName)
-		{
-			if(!Has(logicName))
-				return false;
+      public bool IsEnabled(string logicName)
+      {
+         if(!Has(logicName))
+            return false;
 
-			return bool.Parse(this[logicName]);
-		}
+         return bool.Parse(this[logicName]);
+      }
 
-		public bool Toggle(string logicName)
-		{
-			this[logicName] = IsEnabled(logicName) ? "false" : "true";
-			Save();
-			return bool.Parse(this[logicName]);
-		}
+      public bool Toggle(string logicName)
+      {
+         this[logicName] = IsEnabled(logicName) ? "false" : "true";
+         Save();
+         return bool.Parse(this[logicName]);
+      }
 
-		public void Enable(string logicName)
-		{
-			this[logicName] = "true";
-			Save();
-		}
+      public void Enable(string logicName)
+      {
+         this[logicName] = "true";
+         Save();
+      }
 
-		public void Disable(string logicName)
-		{
-			this[logicName] = "false";
-			Save();
-		}
+      public void Disable(string logicName)
+      {
+         this[logicName] = "false";
+         Save();
+      }
 
-		public void Remove(string logicName)
-		{
-			if(_config != null && logicName.Length > 0)
-			{
-				_config.Remove(logicName);
-				Save();
-			}
-		}
+      public void Remove(string logicName)
+      {
+         if(_config != null && logicName.Length > 0)
+         {
+            _config.Remove(logicName);
+            Save();
+         }
+      }
 
-		public string this[string name]
-		{
-			get
-			{
-				if(!Has(name))
-					return null;
+      public string this[string name]
+      {
+         get
+         {
+            if(!Has(name))
+               return null;
 
-				string value = string.Empty;
+            string value = string.Empty;
 
-				if(_config.TryGetValue(name, out value))
-					return value;
-				else
-					return null;
-			}
-			set
-			{
-				if(_config != null)
-					_config[name] = value;
-				Save();
-			}
-		}
+            if(_config.TryGetValue(name, out value))
+               return value;
+            else
+               return null;
+         }
+         set
+         {
+            if(_config != null)
+               _config[name] = value;
+            Save();
+         }
+      }
 
-		public bool Has(string name)
-		{
-			if(_config == null)
-				return false;
+      public bool Has(string name)
+      {
+         if(_config == null)
+            return false;
 
-			if(_config.Keys.Contains(name))
-				return true;
+         if(_config.Keys.Contains(name))
+            return true;
 
-			return false;
-		}
+         return false;
+      }
 
-		public void Set(string name, object obj)
-		{
-			if(_config != null)
-				_config[name] = (string)obj;// JsonConvert.SerializeObject(obj, Formatting.Indented);
-			Save();
-		}
+      public void Set(string name, object obj)
+      {
+         if(_config != null)
+            _config[name] = (string) obj;// JsonConvert.SerializeObject(obj, Formatting.Indented);
+         Save();
+      }
 
-		public object Get(string name, string def = null)
-		{
-			if(!Has(name))
-				return def;
+      public object Get(string name, string def = null)
+      {
+         if(!Has(name))
+            return def;
 
-			return _config?[name];
-		}
+         return _config?[name];
+      }
 
-		public Config()
-		{
-			if(File.Exists(Configuration.configFilePath))
-			{
-				_config = JsonConvert.DeserializeObject<Dictionary<string, string>>
-					(File.ReadAllText(Configuration.configFilePath));
-				log($"Config()->load {_config.Count()} vars", "debug");
-			}
-		}
+      public Config()
+      {
+         if(File.Exists(Configuration.configFilePath))
+         {
+            _config = JsonConvert.DeserializeObject<Dictionary<string, string>>
+               (File.ReadAllText(Configuration.configFilePath));
+            log($"Config()->load {_config.Count()} vars", "debug");
+         }
+      }
 
-		public int Save()
-		{
-			var data = string.Empty;
+      public int Save()
+      {
+         var data = string.Empty;
 
-			if(_config == null)
-				return -1;
+         if(_config == null)
+            return -1;
 
-			try
-			{
-				data = JsonConvert.SerializeObject(_config, Formatting.Indented);
-				log($"Config()->saving {data.Length} bytes '{data}' to {Configuration.configFilePath}", "debug");
-				File.WriteAllText(Configuration.configFilePath, data);
-			}
-			catch(InvalidOperationException e)
-			{
-				log($"exception in save config: {e.Message}", "error");
-			}
+         try
+         {
+            data = JsonConvert.SerializeObject(_config, Formatting.Indented);
+            log($"Config()->saving {data.Length} bytes '{data}' to {Configuration.configFilePath}", "debug");
+            File.WriteAllText(Configuration.configFilePath, data);
+         }
+         catch(InvalidOperationException e)
+         {
+            log($"exception in save config: {e.Message}", "error");
+         }
 
-			return data.Length;
-		}
+         return data.Length;
+      }
 
-		~Config()
-		{
-			Save();
-		}
+      ~Config()
+      {
+         Save();
+      }
 
-		public string DumpString()
-		{
-			return JsonConvert.SerializeObject(_config, Formatting.Indented);
-		}
+      public string DumpString()
+      {
+         return JsonConvert.SerializeObject(_config, Formatting.Indented);
+      }
 
-		public void Clear()
-		{
-			if(_config != null)
-				_config.Clear();
-			Save();
-		}
-	}
+      public void Clear()
+      {
+         if(_config != null)
+            _config.Clear();
+         Save();
+      }
+   }
 }
